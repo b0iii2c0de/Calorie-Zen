6af96ceb8f645dde67b2b493528ab013d30893e2
c0de b0iii <boiiicode@gmail.com>
2023-06-02 17:24:44 +0300

    feat: start developing calorie counting app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header  from './Header';
+import AppContainer from './AppContainer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppContainer />
+    </>
+  );
+}
+
+export default App;
